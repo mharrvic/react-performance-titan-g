046547bd49b8046dac82576711e3036b743bf6db
mac <mharrvic@gmail.com>
2022-05-20 10:27:03 +0800

    feat: add dashboard components

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import ActivityList from "./components/home/ActivityList";
+import OverviewDashboard from "./components/home/OverviewDashboard";
+import PageHeader from "./components/home/PageHeader";
+import Sidebar from "./components/Sidebar";
+
+export default function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="min-h-full">
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <div className="lg:pl-64 flex flex-col flex-1">
+        <Header setSidebarOpen={setSidebarOpen} />
+        <main className="flex-1 pb-8">
+          <PageHeader />
+          <div className="mt-8">
+            <OverviewDashboard />
+            <ActivityList />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
