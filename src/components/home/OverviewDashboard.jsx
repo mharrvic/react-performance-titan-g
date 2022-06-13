a61cb1f56c0bf69037c4855f19b421bc5a7dfae6
mac <mharrvic@gmail.com>
2022-06-13 18:47:37 +0800

    feat: add time counter

--- src/components/home/OverviewDashboard.jsx
+++ src/components/home/OverviewDashboard.jsx
@@ -66,7 +66,13 @@ export default function OverviewDashboard() {
   };
   return (
     <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-      <h2 className="text-lg leading-6 font-medium text-gray-900">Overview</h2>
+      <h2 className="text-lg leading-6 font-medium text-gray-900">
+        Overview
+        <span className="text-sm text-gray-400">
+          {" "}
+          {time.toLocaleTimeString()}
+        </span>
+      </h2>
       <div className="mt-2 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
         <Card
           balance={balance}
