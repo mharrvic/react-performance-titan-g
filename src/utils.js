899d0bc9f435e9829e4b589328c40456b870728a
mac <mharrvic@gmail.com>
2022-06-19 20:34:51 +0800

    feat: add useForceRerender util

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
+import React from "react";
+
 export function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
+
+export const useForceRerender = () => React.useReducer((x) => x + 1, 0)[1];
